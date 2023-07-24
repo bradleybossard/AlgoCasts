3c86e034d80973a762ed5215799bb578e472c5ef
Bradley Bossard <bradleybossard@gmail.com>
2023-07-24 09:26:11 -0700

    completing maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const counts = {};
+  str.split("").map((char) => {
+    counts[char] = counts[char] ? counts[char] + 1 : 1;
+  });
+  const maxCount = Math.max(Object.values(counts));
+  const maxArray = Object.keys(counts).reduce((acc, key) => {
+    if (counts[key] === maxCount) {
+      acc.push(key);
+    }
+    return acc;
+  });
+  return maxArray[0];
+}
 
 module.exports = maxChar;
